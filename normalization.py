b0c92bd7b70daa1238dd5dc6c27a139109d88ef2
d50776310@gmail.com <d50776310@gmail.com>
2024-09-23 22:02:12 +0300

    m

---
+++ normalization.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+input_file = 'datasets/dataset_modified.csv'
+df = pd.read_csv(input_file)
+
+df['Gender'] = df['Gender'].map({'Male': 1, 'Female': 2})
+
+df.rename(columns={
+    'Calories Burn': 'Каллорий сожжено',
+    'Actual Weight': 'Вес',
+    'Age': 'Возраст',
+    'Gender': 'Пол',
+    'Duration': 'Продолжительность',
+    'Heart Rate': 'Частота сердцебиений',
+    'BMI': 'Индекс массы тела'
+}, inplace=True)
+
+output_file = 'datasets/normalized_data.csv'
+df.to_csv(output_file, index=False)
