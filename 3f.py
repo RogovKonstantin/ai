1f23edf0d0f266f4538c30f7380ae00367a3356c
d50776310@gmail.com <d50776310@gmail.com>
2024-10-07 22:44:38 +0300

    m

--- 3f.py
+++ 3f.py
@@ -4,13 +4,20 @@ import matplotlib.pyplot as plt
 
 def plot_approximating_lines(x, y_clean, y_noisy, theta1_values, title, filename):
     plt.figure(figsize=(10, 6))
+
     for theta1 in theta1_values:
         h_x_clean = theta1 * x
         h_x_noisy = theta1 * x
-        plt.plot(x, h_x_clean, color='blue', alpha=0.2)
-        plt.plot(x, h_x_noisy, color='orange', alpha=0.2)
+
+        # Прямые для чистых данных (синяя сплошная линия)
+        plt.plot(x, h_x_clean, color='blue', alpha=0.7, linewidth=2,
+                 label=f'Чистые данные' if theta1 == theta1_values[0] else "")
+        # Прямые для зашумленных данных (оранжевая пунктирная линия)
+        plt.plot(x, h_x_noisy, color='orange', linestyle='--', alpha=0.7, linewidth=2, label=f'Зашумленные данные' if theta1 == theta1_values[0] else "")
+
     plt.scatter(x, y_clean, color='green', label='Чистые данные')
     plt.scatter(x, y_noisy, color='red', label='Зашумленные данные')
+
     plt.xlim(0, 21)
     plt.xticks(np.arange(0, 22, 1))
     plt.ylim(0, max(np.concatenate((y_clean, y_noisy))) * max(theta1_values) + 1)
@@ -34,8 +41,8 @@ def plot_error_function(x, y_clean, y_noisy, theta1_values, title, filename):
     plt.plot(theta1_values, J_theta1_noisy, label='J(theta1) Зашумленные данные', color='orange')
     plt.scatter(theta1_min_clean, min(J_theta1_clean), color='blue')
     plt.scatter(theta1_min_noisy, min(J_theta1_noisy), color='red')
-    plt.ylim([0,250])
-    plt.xlim([0.5,1.5])
+    plt.ylim([0, 250])
+    plt.xlim([0.5, 1.5])
     plt.title(title)
     plt.xlabel('theta1')
     plt.ylabel('J(theta1)')
@@ -53,9 +60,9 @@ y_noisy = x + np.random.uniform(-2, 2, x.shape)
 theta1_values_line = np.linspace(0, 2, 5)
 theta1_values_error = np.linspace(0, 2, 120)
 
-# Для линий
+# Для прямых
 plot_approximating_lines(x, y_clean, y_noisy, theta1_values_line,
-    "Аппроксимирующие прямые для чистых и зашумленных данных", 'lines.png')
+                         "Аппроксимирующие прямые для чистых и зашумленных данных", 'lines.png')
 
 # Для ошибки
 theta1_min_clean, theta1_min_noisy = plot_error_function(x, y_clean, y_noisy, theta1_values_error,
