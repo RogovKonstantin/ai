5debe5276254fbf2d33b3920a08e6b8a93cb8d23
d50776310@gmail.com <d50776310@gmail.com>
2024-11-01 19:34:11 +0300

    m

--- live-practice/dot_product.py
+++ live-practice/dot_product.py
@@ -6,17 +6,17 @@ col1 = np.array([[1],
 col2 = np.array([[4],
                  [5],
                  [6]])
-
+print("размерности:")
 print(col1.shape)
 print(col2.shape)
 
 vectorized = np.sum(col1 * col2)
-print(f"Скалярное произведение (сумма произведений): {vectorized}")
+print(f"Скалярное произведение (поэлементное произведение): {vectorized}")
 
 byElements = 0
 for i in range(len(col1)):
     byElements += col1[i][0] * col2[i][0]
-print(f"Скалярное произведение (поэлементное произведение): {byElements}")
+print(f"Скалярное произведение (с использованием цикла): {byElements}")
 
 dot_product_vectorized = np.dot(col1.T, col2).item()
-print(f"Скалярное произведение (векторный подход): {dot_product_vectorized}")
+print(f"Скалярное произведение (векторное произведение): {dot_product_vectorized}")
